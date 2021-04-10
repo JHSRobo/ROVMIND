004734e18630e12a9e689e76f68209a5b6d1e13f
ajgrinds <ajgrinds03@gmail.com>
2021-04-10 12:35:00 -0700

    Added chmod

--- ros_workspace/src/electromag/src/electromag.py
+++ ros_workspace/src/electromag/src/electromag.py
@@ -1,11 +1,18 @@
+#!/usr/bin/env python
+
 import RPi.GPIO as GPIO
 import rospy
+from std_msgs.msg import Int32 
 
 
 def toggleElectromag(pin):
+  print("Callback")
+  pin = pin.data
   if pin in pins:
     GPIO.output(pin, int(GPIO.input(pin) == 0))
+    print("Changed pin {}".format(pin))
   else:
+    print("Changed pin {} and added to pins list".format(pin))
     GPIO.setup(pin, GPIO.out)
     pins.append(pin)
     GPIO.output(pin, 1)
@@ -14,10 +21,10 @@ def toggleElectromag(pin):
 
 if __name__ == "__main__":
   GPIO.setmode(GPIO.BOARD)
+  print("Setting up")
   pins = []
+  rospy.init_node("electromag", anonymous=True)
+  rospy.Subscriber("electromag", Int32, toggleElectromag)
 
-  rospy.init_node("electromag")
-
-  rospy.Subscriber("electromag", pin, toggleElectromag)
-
+  rospy.spin()
   GPIO.cleanup()
