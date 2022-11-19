f0e6aa7b73fe5e4b7a308e5a82af4a5e57f2a302
James Randall <Jammerand14@gmail.com>
2022-11-18 20:20:38 -0800

    Removed lots of depricated files, updated tcu build functionality

--- tcu_bringup.sh
+++ tcu_bringup.sh
@@ -13,11 +13,11 @@ apt install curl
 apt install python3-pip
 apt install net-tools
 python3 -m pip install smbus
-mkdir ~/Github
 
 # Edit Ubuntu Files
-echo "alias topside=\"cd /home/jhsrobo/Github/ROVMIND/ros_workspace && rosrun launch_files bottomside.launch\"" >> ~/.bashrc
+echo "alias topside=\"cd /home/jhsrobo/ROVMIND/ros_workspace && rosrun launch_files bottomside.launch\"" >> ~/.bashrc
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+echo "source /home/jhsrobo/ROVMIND/ros_workspace/devel/setup.bash"
 echo "export ROS_MASTER_URI=http://master:11311" >> ~/.bashrc
 echo "export ROS_HOSTNAME=master" >> ~/.bashrc
 echo "export ROS_IP=192.168.1.100" >> ~/.bashrc
@@ -25,7 +25,6 @@ touch /etc/udev/rules.d/joystick.rules
 echo "“KERNAL==“HyACMO” MODE==“06666””" >> /etc/udev/rules.d/joystick.rules
 echo "192.168.1.100 master" >> /etc/hosts
 echo "192.168.1.111 bottomside" >> /etc/hosts
-
 
 #Installing ROS
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
--- tcu_repo_clone.sh
+++ tcu_repo_clone.sh
@@ -6,17 +6,13 @@ if [[ "$(id -u)" == 0 ]]
   exit
 fi
 
-cd ~/Github
-
-rm -rf ./ROVMIND
-git clone --branch V2.13 https://github.com/JHSRobo/ROVMIND.git
-
-cd ~/Github/ROVMIND/ros_workspace/src/
+cd ~/ROVMIND/ros_workspace/src/
 git clone https://github.com/JHSRobo/RPICamera.git
 git clone https://github.com/JHSRobo/copilot_interface.git
 git clone https://github.com/JHSRobo/rov_control_interface.git
+git clone https://github.com/JHSRobo/launch_files.git
 
-cd ~/Github/ROVMIND/ros_workspace
+cd ~/ROVMIND/ros_workspace
 
 rosdep install --from-paths src --ignore-src -r -y
 
