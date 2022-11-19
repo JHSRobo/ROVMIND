62e6edce58d8ac0d6a9e1d5deef964dc0c347c65
James Randall <Jammerand14@gmail.com>
2022-11-18 20:49:56 -0800

    Update tcu_bringup.sh

--- tcu_bringup.sh
+++ tcu_bringup.sh
@@ -8,11 +8,11 @@ if [[ "$(id -u)" != 0 ]]
   exit
 fi
 
-apt install git
-apt install curl
-apt install python3-pip
-apt install net-tools
-python3 -m pip install smbus
+apt update && apt upgrade
+apt install curl -y
+apt install python3-pip -y
+apt install net-tools -y
+python3 -m pip install smbus -y
 
 # Edit Ubuntu Files
 echo "alias topside=\"cd /home/jhsrobo/ROVMIND/ros_workspace && rosrun launch_files bottomside.launch\"" >> ~/.bashrc
@@ -25,9 +25,11 @@ touch /etc/udev/rules.d/joystick.rules
 echo "“KERNAL==“HyACMO” MODE==“06666””" >> /etc/udev/rules.d/joystick.rules
 echo "192.168.1.100 master" >> /etc/hosts
 echo "192.168.1.111 bottomside" >> /etc/hosts
+source ~/.bashrc
 
 #Installing ROS
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+apt update
 apt install ros-noetic-desktop
 source /opt/ros/noetic/setup.bash
 apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
@@ -36,5 +38,3 @@ sudo -u jhsrobo rosdep update
 
 #Clone our software from Github
 sudo -u jhsrobo bash tcu_repo_clone.sh
-
-source ~/.bashrc
