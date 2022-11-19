82045adf55eae7651c9159fbb345da9cdeb6343f
James Randall <Jammerand14@gmail.com>
2022-11-18 23:04:01 -0800

    bug begonezo

--- tcu_bringup.sh
+++ tcu_bringup.sh
@@ -29,11 +29,12 @@ apt install curl -y
 apt install python3-pip -y
 apt install net-tools -y
 python3 -m pip install smbus -y
-apt --fix-broken-install
-apt update && apt upgrade --allow-unauthenticated -y
 
 # Installing ROS
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+apt update && apt upgrade --allow-unauthenticated -y
+apt --fix-broken-install
 apt install ros-noetic-desktop -y
 source /opt/ros/noetic/setup.bash
 apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
