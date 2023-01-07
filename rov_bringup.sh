566e929209fc8d6b0e9e633fd14ea66ce33e7160
James Randall <Jammerand14@gmail.com>
2023-01-07 13:55:15 -0800

    Added ROV Bringup file

---
+++ rov_bringup.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# Check sudo perms
+if [[ "$(id -u)" != 0 ]]
+  then echo "Please run as root"
+  exit
+fi
+
+apt update && apt upgrade -y
+ufw allow 22
+ssh-keygen -t rsa -b 4096 -f /etc/ssh/ssh_host_rsa_key
+
+# Edit Ubuntu Files
+rm -rf /home/jhsrobo/.bashrc
+mv /home/jhsrobo/Github/ROVMIND/bashrc_bottom /home/jhsrobo/.bashrc
+echo "192.168.1.100 master" >> /etc/hosts
+echo "192.168.1.111 bottomside" >> /etc/hosts
+touch /etc/udev/rules.d/60-extra-acl.rules
+echo 'KERNEL=="ttyUSB[0-9]*", TAG+="udev-acl", TAG+="uaccess"'
+source /home/jhsrobo/.bashrc
+
+# Install required packages
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F42ED6FBAB17C654
+  # Above command adds key for ROS update
+apt install curl -y
+apt install python3-pip -y
+apt install net-tools -y
+python3 -m pip install smbus -y
+python3 -m pip install flask -y
+
+# Installing ROS
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+apt update && apt upgrade --allow-unauthenticated -y
+apt --fix-broken-install -y
+apt install ros-noetic-desktop -y
+source /opt/ros/noetic/setup.bash
+apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
+rosdep init smbus
+sudo -u jhsrobo rosdep update
+
+# Create Swap File
+swapoff -a
+dd if=/dev/zero of=/swapfile bs=1M count=8000 status=progress
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+
+# enable I2C
+echo '>>> Enable I2C'
+if grep -q 'i2c-bcm2708' /etc/modules; then
+  echo 'Seems i2c-bcm2708 module already exists, skip this step.'
+else
+  echo 'i2c-bcm2708' >> /etc/modules
+fi
+if grep -q 'i2c-dev' /etc/modules; then
+  echo 'Seems i2c-dev module already exists, skip this step.'
+else
+  echo 'i2c-dev' >> /etc/modules
+fi
+if grep -q 'dtparam=i2c1=on' /boot/config.txt; then
+  echo 'Seems i2c1 parameter already set, skip this step.'
+else
+  echo 'dtparam=i2c1=on' >> /boot/config.txt
+fi
+if grep -q 'dtparam=i2c_arm=on' /boot/config.txt; then
+  echo 'Seems i2c_arm parameter already set, skip this step.'
+else
+  echo 'dtparam=i2c_arm=on' >> /boot/config.txt
+fi
+if [ -f /etc/modprobe.d/raspi-blacklist.conf ]; then
+  sed -i 's/^blacklist spi-bcm2708/#blacklist spi-bcm2708/' /etc/modprobe.d/raspi-blacklist.conf
+  sed -i 's/^blacklist i2c-bcm2708/#blacklist i2c-bcm2708/' /etc/modprobe.d/raspi-blacklist.conf
+else
+  echo 'File raspi-blacklist.conf does not exist, skip this step.'
+fi
+# install i2c-tools
+echo '>>> Install i2c-tools'
+if hash i2cget 2>/dev/null; then
+  echo 'Seems i2c-tools is installed already, skip this step.'
+else
+  apt-get install -y i2c-tools
+fi
+
+# Clone our software from Github
+sudo -u jhsrobo bash tcu_repo_clone.sh
