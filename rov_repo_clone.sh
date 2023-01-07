b643098414769be0069ebcb3e52186f1b2654c8e
James Randall <Jammerand14@gmail.com>
2023-01-07 14:01:06 -0800

    Added ROV Repo Clone

--- rov_repo_clone.sh
+++ rov_repo_clone.sh
@@ -11,12 +11,12 @@ cd /home/jhsrobo/Github/ROVMIND/ros_workspace/src/
 source /opt/ros/noetic/setup.bash
 
 # Remove old repos
-rm -rf RPICamera copilot_interface rov_control launch_files
+rm -rf thrusters depth_sensor gpio_control launch_files
 
 # Clone new repos
-git clone https://github.com/JHSRobo/cameras.git
-git clone https://github.com/JHSRobo/copilot_interface.git -b ROVMIND-Rev-2
-git clone https://github.com/JHSRobo/rov_control.git
+git clone https://github.com/JHSRobo/thrusters.git
+git clone https://github.com/JHSRobo/depth_sensor.git
+git clone https://github.com/JHSRobo/gpio_control.git
 git clone https://github.com/JHSRobo/launch_files.git -b ROVMIND-Rev-2
 
 cd /home/jhsrobo/ROVMIND/ros_workspace
