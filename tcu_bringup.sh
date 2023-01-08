a1579f77443b4dc3eefcbd117bda109e5145435a
James Randall <84990498+James-Randall-14@users.noreply.github.com>
2023-01-07 18:55:34 -0800

    Update tcu_bringup.sh

--- tcu_bringup.sh
+++ tcu_bringup.sh
@@ -8,7 +8,7 @@ fi
 
 # Edit Ubuntu Files
 rm -rf /home/jhsrobo/.bashrc
-mv /home/jhsrobo/Github/ROVMIND/bashrc_top /home/jhsrobo/.bashrc
+mv /home/jhsrobo/ROVMIND/bashrc_top /home/jhsrobo/.bashrc
 touch /etc/udev/rules.d/joystick.rules
 echo "“KERNAL==“HyACMO” MODE==“06666””" >> /etc/udev/rules.d/joystick.rules
 echo "192.168.1.100 master" >> /etc/hosts
@@ -25,15 +25,15 @@ python3 -m pip install smbus -y
 python3 -m pip install flask -y
 
 # Installing ROS
-sudo -u jhsrobo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo -u jhsrobo curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo -u jhsrobo apt update && apt upgrade --allow-unauthenticated -y
-sudo -u jhsrobo apt --fix-broken-install -y
-sudo -u jhsrobo apt install ros-noetic-desktop -y
-sudo -u jhsrobo source /opt/ros/noetic/setup.bash
-sudo -u jhsrobo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
-sudo -u jhsrobo rosdep init
-sudo -u jhsrobo rosdep update
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+apt update && apt upgrade --allow-unauthenticated -y
+apt --fix-broken-install -y
+apt install ros-noetic-desktop -y
+source /opt/ros/noetic/setup.bash
+apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
+rosdep init
+rosdep update
 
 # Clone our software from Github
 sudo -u jhsrobo bash tcu_repo_clone.sh
