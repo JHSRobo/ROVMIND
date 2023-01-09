8e2cd6f2cdb60e5b3851d4baa421956da4c1e3fe
James Randall <84990498+James-Randall-14@users.noreply.github.com>
2023-01-09 15:40:10 -0800

    Update rov_bringup.sh

--- rov_bringup.sh
+++ rov_bringup.sh
@@ -11,7 +11,7 @@ apt update && apt upgrade -y
 # Edit Ubuntu Files
 rm -rf /home/jhsrobo/.bashrc
 mv /home/jhsrobo/ROVMIND/bashrc_bottom /home/jhsrobo/.bashrc
-echo "192.168.1.100 topside" >> /etc/cloud/templates/hosts.debian.tmpl
+echo "192.168.1.100 master" >> /etc/cloud/templates/hosts.debian.tmpl
 echo "192.168.1.111 bottomside" >> /etc/cloud/templates/hosts.debian.tmpl
 touch /etc/udev/rules.d/60-extra-acl.rules
 echo 'KERNEL=="ttyUSB[0-9]*", TAG+="udev-acl", TAG+="uaccess"'
