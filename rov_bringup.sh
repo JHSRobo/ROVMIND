ef9b8822d011f9b79cd262e77c028d2818934d64
James Randall <84990498+James-Randall-14@users.noreply.github.com>
2023-01-14 13:52:14 -0800

    Update rov_bringup.sh

--- rov_bringup.sh
+++ rov_bringup.sh
@@ -12,11 +12,16 @@ apt update && apt upgrade -y
 rm -rf /home/jhsrobo/.bashrc
 rm -rf /home/jhsrobo/ROVMIND/tcu_repo_clone.sh
 rm -rf /home/jhsrobo/ROVMIND/tcu_bringup.sh
-mv /home/jhsrobo/ROVMIND/bashrc_bottom /home/jhsrobo/.bashrc
-echo "192.168.1.100 master" >> /etc/cloud/templates/hosts.debian.tmpl
-echo "192.168.1.111 bottomside" >> /etc/cloud/templates/hosts.debian.tmpl
+cp /home/jhsrobo/ROVMIND/bashrc_bottom /home/jhsrobo/.bashrc
+
+if grep -Fxq "192.168.1.100 master" /etc/cloud/templates/hosts.debian.tmpl
+  then echo "192.168.1.100 master" >> /etc/cloud/templates/hosts.debian.tmpl
+fi
+if grep -Fxq "192.168.1.111 bottomside" /etc/cloud/templates/hosts.debian.tmpl
+  then echo "192.168.1.111 bottomside" >> /etc/cloud/templates/hosts.debian.tmpl
+fi
+
 touch /etc/udev/rules.d/60-extra-acl.rules
-echo 'KERNEL=="ttyUSB[0-9]*", TAG+="udev-acl", TAG+="uaccess"'
 . /home/jhsrobo/.bashrc
 
 # Install required packages
@@ -26,7 +31,9 @@ apt install curl python3-pip net-tools python3-rpi.gpio -y
 python3 -m pip install adafruit-circuitpython-servokit
 
 # Installing ROS
-sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+if grep -Fxq 'deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main' /etc/apt/sources.list.d/ros-latest.list
+  sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+fi
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 apt update && apt upgrade --allow-unauthenticated -y
 apt install ros-noetic-desktop -y
