1a3476630c5d8df816e5697f34b7198b321c1d02
James Randall <84990498+James-Randall-14@users.noreply.github.com>
2023-01-14 13:54:19 -0800

    Update tcu_bringup.sh

--- tcu_bringup.sh
+++ tcu_bringup.sh
@@ -36,7 +36,7 @@ pip install simple-pid # This package will not work with rosdep for whatever rea
 
 # Installing ROS
 if grep -Fxq 'deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main' /etc/apt/sources.list.d/ros-latest.list
-  sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+  then sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 fi
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 apt update && apt upgrade --allow-unauthenticated -y
