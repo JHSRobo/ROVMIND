6e6ef0a50df9ba8e1ec634276e7946c6a80a783d
James Randall <84990498+James-Randall-14@users.noreply.github.com>
2023-01-20 22:47:21 -0800

    Update rov_repo_clone.sh

--- rov_repo_clone.sh
+++ rov_repo_clone.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Checks sudo perms
-if [[ "$(id -u)" == 0 ]]
-  then echo "Please don't run as root"
+# Check sudo perms
+if [[ "$(id -u)" != 0 ]]
+  then echo "Please run as root"
   exit
 fi
 
@@ -16,10 +16,10 @@ cd /home/jhsrobo/ROVMIND/ros_workspace/src/
 rm -rf thrusters depth_sensor gpio_control launch_files
 
 # Clone new repos
-git clone https://github.com/JHSRobo/thrusters.git
-git clone https://github.com/JHSRobo/depth_sensor.git
-git clone https://github.com/JHSRobo/gpio_control.git
-git clone https://github.com/JHSRobo/launch_files.git
+sudo -u jhsrobo git clone https://github.com/JHSRobo/thrusters.git
+sudo -u jhsrobo git clone https://github.com/JHSRobo/depth_sensor.git
+sudo -u jhsrobo git clone https://github.com/JHSRobo/gpio_control.git
+sudo -u jhsrobo git clone https://github.com/JHSRobo/launch_files.git
 
 cd /home/jhsrobo/ROVMIND/ros_workspace
 
